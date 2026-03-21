c0a0d4e8c3a951ba3dcaad328be932ef1ab85a2a
Auric Vente <mprops@protonmail.com>
2026-03-20 19:15:57 -0600

    Mods

--- script.js
+++ script.js
@@ -94,6 +94,7 @@ ws.onmessage = (event) => {
     input_throttle_ms = current_settings.throttle
     unit_duration = current_settings.unit_duration
     zone_info_el.innerText = `${current_zone} (${online_count})`
+    play_warp_drive()
   }
   else if (data.type === `MODAL`) {
     show_modal(data.text)
@@ -524,6 +525,49 @@ function get_theme(zone) {
     word: `hsl(${hue2.toFixed(1)}, ${(70 + random() * 30).toFixed(1)}%, ${(60 + random() * 20).toFixed(1)}%)`,
     sequence: `hsl(${hue3.toFixed(1)}, ${(70 + random() * 30).toFixed(1)}%, ${(60 + random() * 20).toFixed(1)}%)`,
   }
+}
+
+function play_warp_drive() {
+  init_audio()
+
+  let duration = 1.5
+  let start_time = audio_ctx.currentTime
+  let osc_main = audio_ctx.createOscillator()
+  let osc_sub = audio_ctx.createOscillator()
+  let main_gain = audio_ctx.createGain()
+  let low_pass_filter = audio_ctx.createBiquadFilter()
+
+  osc_main.type = `sine`
+  // using a triangle wave instead of sawtooth removes the harsh, buzzy edge
+  osc_sub.type = `triangle`
+  low_pass_filter.type = `lowpass`
+
+  // muffling the high frequencies makes the effect sound deeper and more distant
+  low_pass_filter.frequency.setValueAtTime(400, start_time)
+  low_pass_filter.frequency.exponentialRampToValueAtTime(50, start_time + duration)
+
+  // a lower, smoother frequency sweep for the main tone
+  osc_main.frequency.setValueAtTime(60, start_time)
+  osc_main.frequency.exponentialRampToValueAtTime(600, start_time + 1.2)
+
+  osc_sub.frequency.setValueAtTime(80, start_time)
+  osc_sub.frequency.exponentialRampToValueAtTime(15, start_time + 1.3)
+
+  // softening the volume envelope so it fades in and out gently
+  main_gain.gain.setValueAtTime(0, start_time)
+  main_gain.gain.linearRampToValueAtTime(0.15, start_time + 0.3)
+  main_gain.gain.exponentialRampToValueAtTime(0.01, start_time + duration)
+
+  osc_main.connect(main_gain)
+  osc_sub.connect(main_gain)
+  main_gain.connect(low_pass_filter)
+  low_pass_filter.connect(audio_ctx.destination)
+
+  osc_main.start(start_time)
+  osc_sub.start(start_time)
+
+  osc_main.stop(start_time + duration)
+  osc_sub.stop(start_time + duration)
 }
 
 animate()
