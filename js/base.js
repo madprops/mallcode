675bded8dbbdea0479012fa439b07aa25089574a
Auric Vente <mprops@protonmail.com>
2026-03-29 12:45:01 -0600

    Mods

--- js/base.js
+++ js/base.js
@@ -553,7 +553,7 @@ App.trigger_up = (is_local = true) => {
   let duration = now - App.press_start_time
   App.last_input_time = now
 
-  if (is_local !== false && App.ws && App.ws.readyState === WebSocket.OPEN) {
+  if ((is_local !== false) && App.ws && (App.ws.readyState === WebSocket.OPEN)) {
     App.ws.send(JSON.stringify({type: `UP`, duration}))
   }
 
