573585b95299f321ca0972678507b38258d81eb4
Auric Vente <mprops@protonmail.com>
2026-03-31 22:43:15 -0600

    Mods

--- server/server.js
+++ server/server.js
@@ -374,6 +374,25 @@ App.resolve_word = (zone) => {
 
 App.help_text = `https://www.youtube.com/watch?v=spdfnqS3bDg`
 
+App.get_anomaly_chance = () => {
+  let base_chance = App.anomaly_chance / 100
+  let max_comfortable_anomalies = 10
+  let current_anomalies = Object.values(App.sekrits).filter(s => s.expires).length
+
+  if (current_anomalies >= max_comfortable_anomalies) {
+    return 0
+  }
+
+  let active_users = App.wss.clients.size
+  let user_multiplier = active_users / 3
+
+  if (user_multiplier > 1) {
+    return base_chance / user_multiplier
+  }
+
+  return base_chance
+}
+
 App.process_word = (zone, word, ws) => {
   let sekrit = Object.values(App.sekrits).find(s => s.word === word)
 
@@ -424,7 +443,7 @@ App.process_word = (zone, word, ws) => {
     App.zone_data_changed = true
     App.broadcast_zone_words(zone)
 
-    if (Math.random() < (App.anomaly_chance / 100)) {
+    if (Math.random() < App.get_anomaly_chance()) {
       if (!App.sekrits[word] && !App.shared.is_public_zone(word)) {
         let zone_name = App.shared.random_word(3, word)
         zone_name = zone_name.toUpperCase()
