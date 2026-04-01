665a0225399429bc5aa660f06870b917d87102bf
Auric Vente <mprops@protonmail.com>
2026-03-31 18:46:20 -0600

    Mods

--- server/markov.js
+++ server/markov.js
@@ -1,12 +1,15 @@
 let fs = require(`fs`)
+let path = require(`path`)
 let Markov = require(`markov-strings`).default
 
-function build_corpus(file_path) {
-  let text = fs.readFileSync(file_path, `utf-8`)
+function build_corpus(file_name) {
+  let input_path = path.join(__dirname, file_name)
+  let output_path = path.join(__dirname, `corpus.json`)
+  let text = fs.readFileSync(input_path, `utf-8`)
   let sentences = text.split(`. `)
   let chain = new Markov({stateSize: 2})
   chain.addData(sentences)
-  fs.writeFileSync(`corpus.json`, JSON.stringify(chain.export()))
+  fs.writeFileSync(output_path, JSON.stringify(chain.export()))
 }
 
 build_corpus(`corpus.txt`)
